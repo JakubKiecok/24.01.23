02a4d20d75abcd51db4b366c74728951c4710a69
JanGrzegorski <jan.grzegorski3@gmail.com>
2024-01-23 10:32:25 +0100

    blehh

---
+++ Cstruktury2/Cstruktury2/Cstruktury2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Hobby
+{
+	string nazwa, rodzaj, kierunek;
+};
+
+struct Osoba
+{
+	char imie[50], nazwisko[50];
+	int wiek;
+	Hobby moje_hobby;
+};
+
+int main()
+{
+	Osoba* wsk;
+
+	Osoba o1 = { "Arkadiusz", "Waliczek", 47, {"Programowanie", "JS", "Frontend" } };
+	Osoba o2 = { "Mariusz", "Waliczek", 40, {"Betonowanie", "na sucho", "wykonczeniowka" } };
+	Osoba o3 = { "Zofia", "Waliczek", 30, {"Fryzjerstwo", "trendy", "dredy" } };
+	
+	wsk = &o1;
+	cout << wsk->imie << " " << wsk->nazwisko << " " << wsk->wiek << " " << wsk->moje_hobby.nazwa << " " << wsk->moje_hobby.rodzaj << " " << wsk->moje_hobby.kierunek << endl;
+
+	wsk = &o2;
+	cout << wsk->imie << " " << wsk->nazwisko << " " << wsk->wiek << " " << wsk->moje_hobby.nazwa << " " << wsk->moje_hobby.rodzaj << " " << wsk->moje_hobby.kierunek << endl;
+
+	wsk = &o3;
+	cout << wsk->imie << " " << wsk->nazwisko << " " << wsk->wiek << " " << wsk->moje_hobby.nazwa << " " << wsk->moje_hobby.rodzaj << " " << wsk->moje_hobby.kierunek << endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ Państwa/Państwa/Państwa.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Flaga
+{
+	string kolory;
+	unsigned short int dd, mm, rr;
+};
+
+struct Panstwa
+{
+	string nazwa;
+	int ludnosc, ilosc_wojewodztw;
+	Flaga opis_flagi;
+};
+
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+
+	Panstwa* wsk;
+
+	Panstwa p1{ "Panstwo1", 12458376, 7, {"Czerwony, Czarny", 5, 01, 1292} };
+	Panstwa p2{ "Panstwo2", 12466, 2, {"Niebieski, Zolty, Czarny", 25, 9, 1992} };
+	Panstwa p3{ "Panstwo3", 52376, 45, {"Czerwony, Bialy, Czarny", 12, 10, 2021} };
+	Panstwa p4{ "Panstwo4", 5326734536, 98, {"Czerwony, Niebieski", 8, 2, 1742} };
+	Panstwa p5{ "Panstwo5", 5762930, 15, {"Czerwony, Czarny, Fioletowy, Niebieski, Biały", 20, 12, 1112} };
+
+	int wybiez;
+
+	cout << "Ktore państwo chcesz zobaczyć od 1 do 5." << endl << "Państwo nr: ";
+	cin >> wybiez;
+	switch (wybiez)
+	{
+	case 1:
+		wsk = &p1;
+		cout << "Nazwa " << wsk->nazwa << ", Ludność " << wsk->ludnosc << ", Ilosc wojewodztw " << wsk->ilosc_wojewodztw << ", Kolory flagi " << wsk->opis_flagi.kolory << ", Dzien ustanowienia flagi " << wsk->opis_flagi.dd << " " << wsk->opis_flagi.mm << " " << wsk->opis_flagi.rr << endl;
+		break;
+	case 2:
+		wsk = &p2;
+		cout << "Nazwa " << wsk->nazwa << ", Ludność " << wsk->ludnosc << ", Ilosc wojewodztw " << wsk->ilosc_wojewodztw << ", Kolory flagi " << wsk->opis_flagi.kolory << ", Dzien ustanowienia flagi " << wsk->opis_flagi.dd << " " << wsk->opis_flagi.mm << " " << wsk->opis_flagi.rr << endl;
+		break;
+	case 3:
+		wsk = &p3;
+		cout << "Nazwa " << wsk->nazwa << ", Ludność " << wsk->ludnosc << ", Ilosc wojewodztw " << wsk->ilosc_wojewodztw << ", Kolory flagi " << wsk->opis_flagi.kolory << ", Dzien ustanowienia flagi " << wsk->opis_flagi.dd << " " << wsk->opis_flagi.mm << " " << wsk->opis_flagi.rr << endl;
+		break;
+	case 4:
+		wsk = &p4;
+		cout << "Nazwa " << wsk->nazwa << ", Ludność " << wsk->ludnosc << ", Ilosc wojewodztw " << wsk->ilosc_wojewodztw << ", Kolory flagi " << wsk->opis_flagi.kolory << ", Dzien ustanowienia flagi " << wsk->opis_flagi.dd << " " << wsk->opis_flagi.mm << " " << wsk->opis_flagi.rr << endl;
+		break;
+	case 5:
+		wsk = &p5;
+		cout << "Nazwa " << wsk->nazwa << ", Ludność " << wsk->ludnosc << ", Ilosc wojewodztw " << wsk->ilosc_wojewodztw << ", Kolory flagi " << wsk->opis_flagi.kolory << ", Dzien ustanowienia flagi " << wsk->opis_flagi.dd << " " << wsk->opis_flagi.mm << " " << wsk->opis_flagi.rr << endl;
+		break;
+	default:
+		cout << "Nie ma takiego panstwa w bazie" << endl;
+		break;
+	}
+}
